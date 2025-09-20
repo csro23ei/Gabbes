24419a22599d4dbc8a72e582d6b80656adc017e2
csro23ei <dante030724@gmail.com>
2025-09-20 13:37:13 +0200

    ketegorier och delar sidan fins

--- functions.php
+++ functions.php
@@ -1,9 +1,13 @@
+<?php
 add_action( 'rest_api_init', function() {
+
+    // CORS-hantering
     remove_filter( 'rest_pre_serve_request', 'rest_send_cors_headers' );
     add_filter( 'rest_pre_serve_request', function( $value ) {
-        header( 'Access-Control-Allow-Origin: http://127.0.0.1:5500' ); // bara en
+        header( 'Access-Control-Allow-Origin: http://127.0.0.1:5500' ); // ändra till din domän
         header( 'Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE' );
-        header( 'Access-Control-Allow-Headers: Content-Type, Authorization' ); // viktigt om du använder auth
+        header( 'Access-Control-Allow-Headers: Content-Type, Authorization' );
         return $value;
     });
+
 }, 15 );
